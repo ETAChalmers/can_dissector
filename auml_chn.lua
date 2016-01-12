ac76aa808a8313f2a5f89d5e44fe472429b13d2c
Max Sikstrom <max@pengi.se>
2016-01-12 16:58:26 +0100

    Channel: Handle multiple values per packet
    Since the auml_chn protocol is added multiple times, filters works too
    
    Signed-off-by: Max Sikstrom <max@pengi.se>

--- auml_chn.lua
+++ auml_chn.lua
@@ -1,8 +1,8 @@
 local p_chn = Proto.new("auml_chn", "Auml Channel data")
 
-local f_chn_id = ProtoField.uint8("auml_chn.id", "Channel ID", base.DEC)
+local f_chn_id = ProtoField.uint16("auml_chn.id", "Channel ID", base.DEC)
 local f_chn_src = ProtoField.uint8("auml_chn.src", "Source", base.DEC)
-local f_chn_value = ProtoField.uint8("auml_chn.value", "Value", base.DEC)
+local f_chn_value = ProtoField.uint16("auml_chn.value", "Value", base.DEC)
 
 p_chn.fields = {
     f_chn_id,
@@ -14,22 +14,24 @@ p_chn.fields = {
 local f_can_id = Field.new("can.id")
 
 function p_chn.dissector(tvb,pinfo,root)
-    local subt = root:add(p_chn, tvb)
+    local subt
     local chn_id = f_can_id().tvb:bitfield( 8,16)
     local chn_src = f_can_id().tvb:bitfield(24, 8)
-
-    subt:add(f_chn_id,  chn_id)
-    subt:add(f_chn_src, chn_src)
    
-    -- FIXME: Handle more data fields than one
-    if tvb:len() ~= 2 then return end
-    local chn_value = tvb(0,2)
-    subt:add(f_chn_value, chn_value)
+    local info = "Values:"
+    for i=0,(tvb:len()/2)-1 do
+        local chn_value = tvb(i*2,2)
+        subt = root:add(p_chn, tvb)
+        subt:add(f_chn_id,  chn_id+i)
+        subt:add(f_chn_src, chn_src)
+        subt:add(f_chn_value, chn_value)
+        info = info .. string.format(" %d", chn_value:uint())
+    end
     
     pinfo.cols.protocol = "chn"
     pinfo.cols.src = string.format("node %d", chn_src)
     pinfo.cols.dst = string.format("chn %d", chn_id)
-    pinfo.cols.info = string.format("Value: %d", chn_value:uint())
+    pinfo.cols.info = info
 end
 
 DissectorTable.get("auml.cls"):add(0xa,p_chn)
